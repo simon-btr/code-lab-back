d5b0da1fe979d39680eefdeb7557b59b9f7aba93
7mon <simon.boutrouille123@gmail.com>
2025-08-08 20:13:04 +0200

    Initial commit

---
+++ src/main/java/com/simon/code_lab/CodeLabApplication.java
@@ -0,0 +1,13 @@
+package com.simon.code_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simon/code_lab/CodeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.code_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
